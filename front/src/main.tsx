10a43dba91c1d5fad72b6b587cef2f079ce9478c
javiergp23 <javgon2306@gmail.com>
2024-11-16 21:07:19 -0300

    add useContext

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,20 +3,38 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignUp from './routes/SignUp.tsx'
+import Login from './routes/Login.tsx'
+import Dashboard from './routes/Dashboard.tsx'
+import ProtectedRoute from './routes/ProtectedRoute.tsx'
+import { AuthProvider } from './routes/auth/AuthProvider.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp/>,
+    
   },
   {
     path: '/',
-    element: <App />,
+    element: <ProtectedRoute/>,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Dashboard/>,
+      }
+    ]
   }
 ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
